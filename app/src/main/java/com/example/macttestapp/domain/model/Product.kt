0ac477646bde379a590aaa45e77874dab9c0df5b
RedMuri <celovekda@gmail.com>
2023-07-20 17:12:30 +0300

    Add product entity

---
+++ app/src/main/java/com/example/macttestapp/domain/model/Product.kt
@@ -0,0 +1,10 @@
+package com.example.macttestapp.domain.model
+
+data class Product(
+    val id: Int,
+    val title: String,
+    val price: Int,
+    val thumbnail: String,
+    val description: String,
+    val image: String
+)
