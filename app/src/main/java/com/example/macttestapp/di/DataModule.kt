14b9a76184497cb736a62a7c8e958809cd7259ea
RedMuri <celovekda@gmail.com>
2023-07-20 21:34:37 +0300

    Add dagger data module

---
+++ app/src/main/java/com/example/macttestapp/di/DataModule.kt
@@ -0,0 +1,29 @@
+package com.example.macttestapp.di
+
+import android.app.Application
+import com.example.macttestapp.data.RepositoryImpl
+import com.example.macttestapp.data.network.ApiFactory
+import com.example.macttestapp.data.network.ApiService
+import com.example.macttestapp.domain.Repository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+
+@Module
+interface DataModule {
+
+    @ApplicationScope
+    @Binds
+    fun bindRepository(impl: RepositoryImpl): Repository
+
+    companion object {
+
+        @ApplicationScope
+        @Provides
+        fun provideApiService(
+            application: Application,
+        ): ApiService {
+            return ApiFactory(application).apiService
+        }
+    }
+}
