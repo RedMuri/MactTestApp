95a1b84fcf7c3c022cd48bc6c295fa84f8679e1a
RedMuri <celovekda@gmail.com>
2023-07-20 14:01:10 +0300

    Setup navigation in MainActivity

--- app/src/main/java/com/example/macttestapp/MainActivity.kt
+++ app/src/main/java/com/example/macttestapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.macttestapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+    }
+    private val navController by lazy { navHostFragment.navController }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<NavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
     }
 }
