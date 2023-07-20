e56b068d52fa807d0232fa2082e7cb6f5b834826
RedMuri <celovekda@gmail.com>
2023-07-20 14:47:25 +0300

    Add navigation button to ActionBar

--- app/src/main/java/com/example/macttestapp/MainActivity.kt
+++ app/src/main/java/com/example/macttestapp/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.macttestapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,12 +16,30 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
     }
     private val navController by lazy { navHostFragment.navController }
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigationDrawer()
+    }
 
+    private fun setupNavigationDrawer() {
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.quotesFragment,
+                R.id.itemsFragment,
+                R.id.settingsFragment
+            ), findViewById(R.id.drawer_layout)
+        )
+        setupActionBarWithNavController(
+            navController, appBarConfiguration
+        )
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)|| super.onSupportNavigateUp()
     }
 }
